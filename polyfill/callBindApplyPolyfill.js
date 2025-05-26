c02e2092937e1000a3e26b736dbd33e78ab7e1cc
prasha111 <prashantredeem@gmail.com>
2025-05-26 16:15:20 +0530

    apply polyfill

--- polyfill/callBindApplyPolyfill.js
+++ polyfill/callBindApplyPolyfill.js
@@ -14,3 +14,12 @@ Function.prototype.myBind = function (context, ...args){
         return  context[fnSy].apply([...newArgs  ,...args])
     }
 }
+Function.prototype.myApply = function(context,  args){
+    context = context || globalThis;
+    let fbSy = new Symbol();
+    context[fnSy] = this;
+    let result  = context[fnSy](...(args || []))
+    
+    delete context[fnSy]
+    return result;
+}
